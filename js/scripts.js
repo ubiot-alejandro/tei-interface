764eeeb2625b26b18bf4d30cfe408dede511fb2a
ubiot-alejandro <ubiot.alejandro@gmail.com>
2021-09-20 10:03:57 -0400

    Updated buttons and build authentication

--- js/scripts.js
+++ js/scripts.js
@@ -7,42 +7,44 @@ function myFunction() {
     }
 }
 
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  userFullName = profile.getName();
-  userId = profile.getId();     
-  console.log('Full Name: ' + profile.getName());
-  console.log("Email: " + profile.getEmail());
-  console.log(userId);
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  userToken = id_token;
-};
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  //userId = 123456789
-  auth2.signOut().then(function () {
-      logMessage('User signed out.');
-      logMessage('');
-  });
-  window.location.reload();
-}
-
 function invokeON() {
-  var url="https://5wqbphn9p1.execute-api.us-east-2.amazonaws.com/default/on-function" + "?id=" + userId;
-  fetch(url, {mode:'no-cors'})
-  .then(data=>{return data.json})
-  .then(res=>{console.log(res)})
+
+  let token = window.location.href
+
+  let auth = token.split('&')[1].split('=')[1]
+
+  let url="https://vdawi12xzl.execute-api.eu-central-1.amazonaws.com/default/on-function";
+    
+    fetch(url, {
+      mode: 'cors',
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : auth
+      }
+    })
+    .then(data=>{return data.json})
+    .then(res=>{console.log(res)});
 }
 
 function invokeOFF() {    
-  var url= "https://7346aseqs8.execute-api.us-east-2.amazonaws.com/default/off-function" + "?id=" + userId;
-  fetch(url, {mode:'no-cors'})
-  .then(data=>{return data.json})
-  .then(res=>{console.log(res)})
+  
+  let token = window.location.href
+
+  let auth = token.split('&')[1].split('=')[1]
+  
+  let url="https://8hf8zi8mvd.execute-api.eu-central-1.amazonaws.com/default/off-function";
+    
+    fetch(url, {
+      mode: 'cors',
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : auth
+      }
+    })
+    .then(data=>{return data.json})
+    .then(res=>{console.log(res)});
 }
 
 function updateImage() {
@@ -58,17 +60,18 @@ function updateImage() {
 }
 
 function handleError(evt) {
-  if (evt.message) { // Chrome sometimes provides this
-    //alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
-    alert("Sorry! Please Login with an authenticated user...")
-  }
-  // else {
-  //  alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
-  //}
-}
+  if (evt.message) {
 
-function logMessage(message){
-  $('#log').append(message + '</br>');
+    console.log(evt.message)
+
+    if (evt.message == `Uncaught TypeError: Cannot read properties of undefined (reading 'split')`) {
+
+      alert("Por favor inicie sesion antes...")
+      window.location.href = "https://tei.auth.eu-central-1.amazoncognito.com/login?client_id=7g3i7fpuuotl1d7jjevu29pilq&response_type=token&scope=email+https://tei.logo.actions/logo.off+https://tei.logo.actions/logo.on+openid&redirect_uri=https://d30a2flcb4p1wx.cloudfront.net";
+
+    }
+
+  }
 }
 
 setInterval(updateImage, 1000);
